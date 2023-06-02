75f9f24ccceac490361c4cecd21cc1fc3831e4f8
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-02 18:27:52 +0300

    feat: hide keyboard when tap anywhere

--- App.js
+++ App.js
@@ -1,32 +1,41 @@
 import { useState } from "react"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar as SB } from 'react-native';
+import { StyleSheet, Keyboard, SafeAreaView, Platform, StatusBar as SB, TouchableWithoutFeedback } from 'react-native';
 import { colors } from './src/utils/colors';
 import { Focus } from './src/features/Focus';
 import { Timer } from "./src/features/Timer";
 import { FocusHistory } from "./src/features/FocusHistory";
 import { RoundedButton } from "./src/components/RoundedButton";
 
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+)
+
 export default function App() {
   const [currentSubject, setCurrentSubject] = useState(null)
   const [history, setHistory] = useState([])
 
+
   return (
-    <SafeAreaView style={styles.container}>
-      {!currentSubject ? (
-        <>
-          <Focus addSubject={setCurrentSubject} />
-          <FocusHistory history={history} />
-          {history.length > 0 && <RoundedButton style={styles.clearButton} size={80} title="Clear" onPress={() => setHistory([])} />}
-        </>
-      ) : (
-        <Timer
-          focusSubject={currentSubject}
-          onTimerEnd={(subject) => setHistory([...history, subject])}
-          clearSubject={() => setCurrentSubject(null)}
-        />)}
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <DismissKeyboard>
+      <SafeAreaView style={styles.container}>
+        {!currentSubject ? (
+          <>
+            <Focus addSubject={setCurrentSubject} />
+            <FocusHistory history={history} />
+            {history.length > 0 && <RoundedButton style={styles.clearButton} size={80} title="Clear" onPress={() => setHistory([])} />}
+          </>
+        ) : (
+          <Timer
+            focusSubject={currentSubject}
+            onTimerEnd={(subject) => setHistory([...history, subject])}
+            clearSubject={() => setCurrentSubject(null)}
+          />)}
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </DismissKeyboard>
   );
 }
 
